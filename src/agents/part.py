0a644399d22e689a9453f0c20f7948f136f6a50e
AlvaroViamont <alvaro.viamont.seraphys@gmail.com>
2024-09-07 00:55:14 -0400

    Add agents and data structures

--- src/agents/part.py
+++ src/agents/part.py
@@ -1,5 +1,16 @@
 
 
 class Part:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, part_type:str= None, weight:float=None, size:list=None) -> None:
+        self.part_type = part_type
+        self.weight = weight
+        self.size = size
+    
+    def get_weight(self) -> float:
+        return self.weight
+    
+    def get_size(self) -> list:
+        return self.size
+    
+    def __str__(self) -> str:
+        return self.part_type
